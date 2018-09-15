e18e12c8bb4a8ed972ce66bb9c00791948e51091
Panachai <panachai.ny@gmail.com>
2018-09-15 23:17:27 +0700

    Add func JWT and Model login

---
+++ JWT_LEARN/Controllers/AuthenticationController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+using JWT_LEARN.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+
+namespace JWT_LEARN.Controllers
+{
+    [Route("Auth")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult RequestToken([FromBody] TokenRequestModel request)
+        {
+            if (request.Username == "Jon" && request.Password == "Demo")
+            {
+                var claims = new[]
+                {
+                        new Claim(ClaimTypes.Name, request.Username)
+                    };
+
+                var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Environment.GetEnvironmentVariable("SecurityKey")));
+                var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+                var token = new JwtSecurityToken(
+                    issuer: "yourdomain.com",
+                    audience: "yourdomain.com",
+                    claims: claims,
+                    expires: DateTime.Now.AddMinutes(30),
+                    signingCredentials: creds);
+
+                return Ok(new
+                {
+                    token = new JwtSecurityTokenHandler().WriteToken(token)
+                });
+            }
+
+            return BadRequest("Could not verify username and password");
+        }
+    }
+}
